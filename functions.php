6f2f5ab4c837f89ff2384d0e9299490bf0aca6b5
Denzel Chia <email@denzelchia.com>
2016-07-22 17:03:38 +0800

    Initial commit

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+function capsule_child_scripts() {
+	wp_enqueue_style( 'capsule_child-fonts', capsule_child_fonts_url(), array(), null );
+	wp_enqueue_style( 'capsule-child-custom', get_stylesheet_uri(),array('capsule_styles','chosen'));
+}
+add_action( 'wp_enqueue_scripts', 'capsule_child_scripts' );
+
+if ( ! function_exists( 'capsule_child_fonts_url' ) ) :
+function capsule_child_fonts_url() {
+	$fonts_url = '';
+	$fonts     = array();
+	$subsets   = 'latin,latin-ext';
+
+//	/* translators: If there are characters in your language that are not supported by Merriweather, translate this to 'off'. Do not translate into your own language. */
+//	if ( 'off' !== _x( 'on', 'Open Sans font: on or off', 'capsule' ) ) {
+//		$fonts[] = 'Open Sans:400,700,900,400italic,700italic,900italic';
+//	}
+
+	/* translators: If there are characters in your language that are not supported by Montserrat, translate this to 'off'. Do not translate into your own language. */
+	if ( 'off' !== _x( 'on', 'Montserrat font: on or off', 'capsule_child' ) ) {
+		$fonts[] = 'Montserrat:400,700';
+	}
+
+	/* translators: If there are characters in your language that are not supported by Inconsolata, translate this to 'off'. Do not translate into your own language. */
+	if ( 'off' !== _x( 'on', 'Inconsolata font: on or off', 'capsule_child' ) ) {
+		$fonts[] = 'Inconsolata:400';
+	}
+
+	if ( $fonts ) {
+		$fonts_url = add_query_arg( array(
+			'family' => urlencode( implode( '|', $fonts ) ),
+			'subset' => urlencode( $subsets ),
+		), 'https://fonts.googleapis.com/css' );
+	}
+
+	return $fonts_url;
+}
+endif;
